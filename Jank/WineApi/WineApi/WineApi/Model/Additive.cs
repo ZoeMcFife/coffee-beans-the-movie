80362db7f0ea73d390a7987a453bcca5ddef23a4
Zoe McFife <cmdrprotogen27@gmail.com>
2025-01-05 12:01:04 +0100

    removed unused time field

--- Jank/WineApi/WineApi/WineApi/Model/Additive.cs
+++ Jank/WineApi/WineApi/WineApi/Model/Additive.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Type { get; set; } // Type of additive (e.g., Schwefel, Säure)
         public DateTime Date { get; set; } // Datum
-        public string Time { get; set; } // Uhrzeit
         public float AmountGrammsPerLitre { get; set; } // Menge (gr/L, gr/Hekto, gr/1000)
         public float AmountGrammsPerHectoLitre { get; set; } // Menge (gr/L, gr/Hekto, gr/1000)
         public float AmountGrammsPer1000Litre { get; set; } // Menge (gr/L, gr/Hekto, gr/1000)
--- Jank/WineApi/WineApi/WineApi/Model/DTO/AdditiveDTO.cs
+++ Jank/WineApi/WineApi/WineApi/Model/DTO/AdditiveDTO.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Type { get; set; } // Type of additive (e.g., Schwefel, Säure)
         public DateTime Date { get; set; } // Datum
-        public string Time { get; set; } // Uhrzeit
         public float AmountGrammsPerLitre { get; set; } // Menge (gr/L, gr/Hekto, gr/1000)
         public float AmountGrammsPerHectoLitre { get; set; } // Menge (gr/L, gr/Hekto, gr/1000)
         public float AmountGrammsPer1000Litre { get; set; } // Menge (gr/L, gr/Hekto, gr/1000)
@@ -21,7 +20,6 @@
                 Id = dto.Id,
                 Type = dto.Type,
                 Date = dto.Date,
-                Time = dto.Time,
                 AmountGrammsPerLitre = dto.AmountGrammsPerLitre,
                 AmountGrammsPerHectoLitre = dto.AmountGrammsPerHectoLitre,
                 AmountGrammsPer1000Litre = dto.AmountGrammsPer1000Litre,
@@ -38,7 +36,6 @@
                 Id = additive.Id,
                 Type = additive.Type,
                 Date = additive.Date,
-                Time = additive.Time,
                 AmountGrammsPerLitre = additive.AmountGrammsPerLitre,
                 AmountGrammsPerHectoLitre = additive.AmountGrammsPerHectoLitre,
                 AmountGrammsPer1000Litre = additive.AmountGrammsPer1000Litre,
