977d0b459b1adca772b60543f6e8588938cee882
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2025-01-08 12:51:08 +0100

    testing

--- Jank/WineApi/WineApi/WineApi/obj/Debug/net8.0/WineApi.AssemblyInfo.cs
+++ Jank/WineApi/WineApi/WineApi/obj/Debug/net8.0/WineApi.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WineApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b91cb3c5d2d0075e8dae912ac78418068a19d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711104a0ba41d4530ea82ae709466098ab6da042")]
 [assembly: System.Reflection.AssemblyProductAttribute("WineApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WineApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
